0a6f1baed8c0c31414bce998a4fc16e0e6f7a869
Danny <dnioko0412@gmail.com>
2024-12-02 11:40:35 +0800

    added company and applicant model

--- backend/models/job.model.js
+++ backend/models/job.model.js
@@ -46,5 +46,5 @@ const jobSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Application'
     }]
-});
+}, {timestamps: true});
 export const Job = mongoose.model('Job', jobSchema);
